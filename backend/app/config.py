20790c89947a399214fbbf952cbf025bda9d3f68
sholok20 <sholok192000@gmail.com>
2026-06-28 09:48:06 +0300

    read config from environment variables

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,11 +1,17 @@
+import os
 from pydantic import BaseModel
 
 
 class Settings(BaseModel):
-    DATABASE_URL: str = "postgresql://postgres:12345@localhost:5432/graduation_project"
-    SECRET_KEY: str = "super_secret_key"
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    DATABASE_URL: str = os.getenv(
+        "DATABASE_URL",
+        "postgresql://postgres:12345@localhost:5432/graduation_project"
+    )
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "super_secret_key")
+    ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(
+        os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60")
+    )
 
 
 settings = Settings()
